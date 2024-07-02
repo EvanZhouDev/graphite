88979b0ef38d041f5eaba54e3908f752ce012f46
Evan Zhou <evanzhoudev@gmail.com>
2024-07-02 15:48:33 -0700

    💄 Set up tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				surface: "#FFF",
+				"surface-container": "#F8FAFD",
+				primary: "#1C73E8",
+				"primary-container": "#F4F6FC",
+				secondary: "#D2E3FC",
+				"secondary-container": "#F0F4F8",
+				"secondary-container-hover": "#DDE3EA",
+				dim: "#A6A6A6",
+			},
+		},
+	},
+	plugins: [],
 };
