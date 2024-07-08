c068f81cac9d695df79d82efb0a5d51372cc55c0
Evan Zhou <evanzhoudev@gmail.com>
2024-07-07 20:59:07 -0700

    🥅 Add UI to catch edge cases

--- app/components/chat/index.js
+++ app/components/chat/index.js
@@ -15,77 +15,108 @@ export default function Chat() {
 		setToolHistory,
 		storage,
 	} = useContext(FileContext);
+
+	if (storage.files.length == 0) {
+		return (
+			<div className="flex flex-col justify-center h-full grayscale pointer-events-none items-center">
+				<EmptyView setInput={() => {}} />
+				<div className="mb-12 font-medium">
+					Create a file to use Graphite Chat.
+				</div>
+			</div>
+		);
+	}
+
 	let { memory, content } = file;
+
+	if (storage.apiKey == "" || storage.apiKey == undefined) {
+		return (
+			<div className="flex flex-col justify-center h-full grayscale pointer-events-none items-center">
+				<EmptyView setInput={() => {}} />
+				<div className="mb-12 font-medium">
+					Set your API Key in the API Key tab to use Graphite Chat.
+				</div>
+			</div>
+		);
+	}
 
 	let objectEquals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
 
-	const { messages, input, setInput, setMessages, handleSubmit, isLoading } =
-		useChat({
-			maxToolRoundtrips: 0,
-			initialMessages: [...file.messages],
-			async onToolCall({ toolCall }) {
-				if (toolCall.toolName === "setMemory") {
-					setFile((file) => ({
-						...file,
-						toolHistory: [
-							...file.toolHistory,
-							{
-								...toolCall,
-								updateMap: {
-									formality: toolCall.args.formality != file.memory.formality,
-									intent: !objectEquals(
-										toolCall.args.intent.map((x) =>
-											x.replaceAll("\\n", "\n").replace(/\\/g, "")
-										),
-										file.memory.intent
+	const {
+		error,
+		messages,
+		input,
+		setInput,
+		setMessages,
+		handleSubmit,
+		isLoading,
+	} = useChat({
+		maxToolRoundtrips: 0,
+		initialMessages: [...file.messages],
+		async onToolCall({ toolCall }) {
+			if (toolCall.toolName === "setMemory") {
+				setFile((file) => ({
+					...file,
+					toolHistory: [
+						...file.toolHistory,
+						{
+							...toolCall,
+							updateMap: {
+								formality: toolCall.args.formality != file.memory.formality,
+								intent: !objectEquals(
+									toolCall.args.intent.map((x) =>
+										x.replaceAll("\\n", "\n").replace(/\\/g, "")
 									),
-									audience: !objectEquals(
-										toolCall.args.audience.map((x) =>
-											x.replaceAll("\\n", "\n").replace(/\\/g, "")
-										),
-										file.memory.audience
+									file.memory.intent
+								),
+								audience: !objectEquals(
+									toolCall.args.audience.map((x) =>
+										x.replaceAll("\\n", "\n").replace(/\\/g, "")
 									),
-									summary: !objectEquals(
-										toolCall.args.summary
-											.replaceAll("\\n", "\n")
-											.replace(/\\/g, ""),
-										file.memory.summary
-									),
-								},
+									file.memory.audience
+								),
+								summary: !objectEquals(
+									toolCall.args.summary
+										.replaceAll("\\n", "\n")
+										.replace(/\\/g, ""),
+									file.memory.summary
+								),
 							},
-						],
-						memory: {
-							formality: toolCall.args.formality,
-							intent: structuredClone(toolCall.args.intent).map((x) =>
-								x.replaceAll("\\n", "\n").replace(/\\/g, "")
-							),
-							audience: structuredClone(toolCall.args.audience).map((x) =>
-								x.replaceAll("\\n", "\n").replace(/\\/g, "")
-							),
-							summary: toolCall.args.summary
-								.replaceAll("\\n", "\n")
-								.replace(/\\/g, ""),
 						},
-					}));
+					],
+					memory: {
+						formality: toolCall.args.formality,
+						intent: structuredClone(toolCall.args.intent).map((x) =>
+							x.replaceAll("\\n", "\n").replace(/\\/g, "")
+						),
+						audience: structuredClone(toolCall.args.audience).map((x) =>
+							x.replaceAll("\\n", "\n").replace(/\\/g, "")
+						),
+						summary: toolCall.args.summary
+							.replaceAll("\\n", "\n")
+							.replace(/\\/g, ""),
+					},
+				}));
+			}
+			if (toolCall.toolName === "giveSuggestions") {
+				try {
+					setToolHistory((history) => {
+						let cleanToolCall = structuredClone(toolCall);
+						cleanToolCall.args.corrections = cleanToolCall.args.corrections.map(
+							(x) => ({
+								...x,
+								text: x.text.replaceAll("\\n", "\n").replace(/\\/g, ""),
+								anchor: x.anchor.replaceAll("\\n", "\n").replace(/\\/g, ""),
+							})
+						);
+						return [...history, cleanToolCall];
+					});
+				} catch (e) {
+					console.log(e);
 				}
-				if (toolCall.toolName === "giveSuggestions") {
-					try {
-						setToolHistory((history) => {
-							let cleanToolCall = structuredClone(toolCall);
-							cleanToolCall.args.corrections =
-								cleanToolCall.args.corrections.map((x) => ({
-									...x,
-									text: x.text.replaceAll("\\n", "\n").replace(/\\/g, ""),
-									anchor: x.anchor.replaceAll("\\n", "\n").replace(/\\/g, ""),
-								}));
-							return [...history, cleanToolCall];
-						});
-					} catch (e) {
-						console.log(e);
-					}
-				}
-			},
-		});
+			}
+		},
+	});
 
 	useEffect(() => {
 		if (messages.length > 0) {
--- app/components/editor/index.js
+++ app/components/editor/index.js
@@ -2,36 +2,91 @@ import { useContext } from "react";
 import { FileContext } from "@/providers/file.js";
 import EditorBody from "./editorBody";
 import EditableText from "./editableText";
+import { useRouter } from "next/navigation";
 
 export default function Editor() {
-	let { file } = useContext(FileContext);
+	let router = useRouter();
+	let { file, storage, setStorage } = useContext(FileContext);
 
 	return (
 		<div className="flex flex-col justify-stretch h-full">
-			<div className="flex p-3 pb-0 justify-between">
-				<div className="flex flex-row items-center">
-					<EditableText />
-					<span className="text-dim ml-3 flex items-center">
-						<span className="text-[20px] material-symbols-outlined mr-1">
-							sync_saved_locally
-						</span>
-						Saved Locally
+			{storage.files.length === 0 ? (
+				<div className="w-full h-full flex items-center justify-center flex-col">
+					<span class="material-symbols-outlined text-dim text-[150px] mb-5">
+						indeterminate_question_box
 					</span>
+					<div className="text-3xl font-medium">No Files to Edit</div>
+					<div className="text-dim text-center my-3">
+						Create a new file with the button below,
+						<br />
+						or go to the library to create a file.
+					</div>
+					<div className="flex flex-row gap-3 mt-5">
+						<button
+							className="bg-primary p-3 text-white dark:text-black font-medium text-lg rounded-full px-4 hover:bg-primary-hover active:bg-primary-active transition-colors"
+							onClick={() => {
+								setStorage((storage) => {
+									return {
+										currentFile: storage.files.length,
+										files: [
+											...storage.files,
+											{
+												title: "Untitled File",
+												content: "",
+												messages: [],
+												memory: {
+													formality: 0,
+													audience: [],
+													intent: [],
+													summary: "",
+												},
+												toolHistory: [],
+											},
+										],
+									};
+								});
+							}}
+						>
+							Create New File
+						</button>
+						<button
+							className="border-[1px] border-primary p-3 text-primary font-medium text-lg rounded-full px-4 hover:bg-surface-container active:bg-surface-container-hover transition-colors"
+							onClick={() => {
+								router.push("/library");
+							}}
+						>
+							Go to Library
+						</button>
+					</div>
 				</div>
-				<div className="flex items-center mr-2">
-					<button
-						className="material-symbols-outlined text-dim"
-						onClick={() => {
-							navigator.clipboard.writeText(file.content);
-						}}
-					>
-						file_copy
-					</button>
-				</div>
-			</div>
-			<div className="flex-grow">
-				<EditorBody />
-			</div>
+			) : (
+				<>
+					<div className="flex p-3 pb-0 justify-between">
+						<div className="flex flex-row items-center">
+							<EditableText />
+							<span className="text-dim ml-3 flex items-center">
+								<span className="text-[20px] material-symbols-outlined mr-1">
+									sync_saved_locally
+								</span>
+								Saved Locally
+							</span>
+						</div>
+						<div className="flex items-center mr-2">
+							<button
+								className="material-symbols-outlined text-dim"
+								onClick={() => {
+									navigator.clipboard.writeText(file.content);
+								}}
+							>
+								file_copy
+							</button>
+						</div>
+					</div>
+					<div className="flex-grow">
+						<EditorBody />
+					</div>
+				</>
+			)}
 		</div>
 	);
 }
