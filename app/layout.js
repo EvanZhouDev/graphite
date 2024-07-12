16a61c18977233704c60412e8168b8f0a84727a6
Evan Zhou <evanzhoudev@gmail.com>
2024-07-12 13:06:26 -0700

    📈 Added Vercel Web Analytics

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import "./globals.css";
 import Sidebar from "@/components/sidebar/";
 import ThemeProvider from "./providers/theme.js";
 import OnboardingRouter from "./onboardingRouter";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
