dbc75528a20d0f89851c3a0a41755866f2ea0713
Evan Zhou <evanzhoudev@gmail.com>
2024-08-08 13:56:11 -0700

    🔧 Made blocking threshold slightly higher

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -20,19 +20,19 @@ export async function POST(req) {
 			safetySettings: [
 				{
 					category: "HARM_CATEGORY_HATE_SPEECH",
-					threshold: "BLOCK_NONE",
+					threshold: "BLOCK_ONLY_HIGH",
 				},
 				{
 					category: "HARM_CATEGORY_DANGEROUS_CONTENT",
-					threshold: "BLOCK_NONE",
+					threshold: "BLOCK_ONLY_HIGH",
 				},
 				{
 					category: "HARM_CATEGORY_HARASSMENT",
-					threshold: "BLOCK_NONE",
+					threshold: "BLOCK_ONLY_HIGH",
 				},
 				{
 					category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-					threshold: "BLOCK_NONE",
+					threshold: "BLOCK_ONLY_HIGH",
 				},
 			],
 		}),
